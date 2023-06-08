b009f4b2821b2fa993b32e68dcc2438afbe93ef7
kimgura <krkrdudwn@naver.com>
2023-06-08 12:57:14 +0900

    Step05Final_update 예제 추가

--- Step05Final/src/main/java/test/users/dao/UsersDao.java
+++ Step05Final/src/main/java/test/users/dao/UsersDao.java
@@ -9,17 +9,19 @@ import test.users.dto.UsersDto;
 import test.util.DbcpBean;
 
 public class UsersDao {
+	
 	private static UsersDao dao;
-	// 외부에서 객체 생성할 수 없도록
+	//외부에서 객체 생성할수 없도록
 	private UsersDao() {}
-	// 참조값을 리턴해주는 static 메소드
+	//참조값을 리턴해주는 static 메소드
 	public static UsersDao getInstance() {
 		if(dao==null) {
 			dao=new UsersDao();
 		}
 		return dao;
 	}
-	// 비밀번호를 수정하는 메소드
+	
+	//비밀번호를 수정하는 메소드
 	public boolean updatePwd(UsersDto dto) {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
@@ -53,7 +55,8 @@ public class UsersDao {
 			return false;
 		}
 	}
-	// 프로필 이미지 경로를 수정하는 메소드
+	
+	//프로필 이미지 경로를 수정하는 메소드
 	public boolean updateProfile(UsersDto dto) {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
@@ -87,26 +90,27 @@ public class UsersDao {
 			return false;
 		}
 	}
-	// 회원 한명의 정보를 리턴하는 메소드
+	
+	//회원 한명의 정보를 리턴하는 메소드
 	public UsersDto getData(String id) {
 		UsersDto dto=null;
-		// 필요한 객체의 참조 값을 담을 지역 변수 미리 만들기
+		//필요한 객체의 참조값을 담을 지역변수 미리 만들기
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		try {
-			// DbcpBean 객체를 이용해서 Connection 객체를 얻어온다(Connection Pool에서 얻어오기)
+			//DbcpBean 객체를 이용해서 Connection 객체를 얻어온다(Connection Pool 에서 얻어오기)
 			conn = new DbcpBean().getConn();
-			// 실행할 sql 문
+			//실행할 sql 문 
 			String sql = "SELECT pwd, email, profile, regdate"
 					+ " FROM users"
 					+ " WHERE id=?";
 			pstmt = conn.prepareStatement(sql);
-			// sql 문이 미완성이라면 여기서 완성
+			//sql 문이 미완성이라면 여기서 완성
 			pstmt.setString(1, id);
-			// select 문 수행하고 결과 값 받아오기
+			//select 문 수행하고 결과값 받아오기
 			rs = pstmt.executeQuery();
-			// 반복문 돌면서 ResultSet에 담긴 내용 추출
+			//반복문 돌면서 ResultSet 에 담긴 내용 추출
 			while (rs.next()) {
 				dto=new UsersDto();
 				dto.setId(id);
@@ -124,71 +128,72 @@ public class UsersDao {
 				if (pstmt != null)
 					pstmt.close();
 				if (conn != null)
-					conn.close(); // Connection이 Connection Pool에 반납된다.
+					conn.close(); //Connection 이 Connection Pool 에 반납된다.
 			} catch (Exception e) {
 			}
 		}
 		return dto;
 	}
+	
 	//인자로 전달되는 dto 에 있는 아이디와, 비밀번호를 이용해서 해당 정보가 유효한 정보인지 여부를 리턴하는 메소드 
 	public boolean isValid(UsersDto dto) {
-	      
-	   //아이디 비밀번호 유효성 여부를 담을 변수 만들고 초기값 false 부여하기
-	   boolean isValid=false;
-	      
-	   //필요한 객체를 담을 지역변수를 미리 만들어 둔다. 
-	   Connection conn = null;
-	   PreparedStatement pstmt = null;
-	   ResultSet rs = null;
-	   try {
-	      //Connection Pool 에서 Connection 객체를 하나 얻어온다.
-	      conn = new DbcpBean().getConn();
-	      //실행할 sql 문의 뼈대 구성하기
-	      String sql = "SELECT id"
-	            + " FROM users"
-	            + " WHERE id=? AND pwd=?";
-	      //sql 문의 ? 에 바인딩 할게 있으면 한다
-	      pstmt = conn.prepareStatement(sql);
-	      pstmt.setString(1, dto.getId());
-	      pstmt.setString(2, dto.getPwd());
-	      //SELECT 문을 수행하고 결과값을 받아온다.
-	      rs = pstmt.executeQuery();
-	      //SELECT 된 ROW 가 있는지 확인해 본다.
-	      if (rs.next()) {
-	         //SELECT 된 row 가 있다면 유효한 정보가 맞다.
-	         isValid=true;
-	      }
-	   } catch (Exception e) {
-	      e.printStackTrace();
-	   } finally {
-	      try {
-	         if (rs != null)
-	            rs.close();
-	         if (pstmt != null)
-	            pstmt.close();
-	         if (conn != null)
-	            conn.close(); //Connection Pool 에 Connection 반납하기
-	      } catch (Exception e) {
-	      }
-	   }
-	   return isValid;
+		
+		//아이디 비밀번호 유효성 여부를 담을 변수 만들고 초기값 false 부여하기
+		boolean isValid=false;
+		
+		//필요한 객체를 담을 지역변수를 미리 만들어 둔다. 
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			//Connection Pool 에서 Connection 객체를 하나 얻어온다.
+			conn = new DbcpBean().getConn();
+			//실행할 sql 문의 뼈대 구성하기
+			String sql = "SELECT id"
+					+ " FROM users"
+					+ " WHERE id=? AND pwd=?";
+			//sql 문의 ? 에 바인딩 할게 있으면 한다
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, dto.getId());
+			pstmt.setString(2, dto.getPwd());
+			//SELECT 문을 수행하고 결과값을 받아온다.
+			rs = pstmt.executeQuery();
+			//SELECT 된 ROW 가 있는지 확인해 본다.
+			if (rs.next()) {
+				//SELECT 된 row 가 있다면 유효한 정보가 맞다.
+				isValid=true;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close(); //Connection Pool 에 Connection 반납하기
+			} catch (Exception e) {
+			}
+		}
+		return isValid;
 	}	
 	
-	// 회원 한명의 정보를 DB에 저장하고 성공 여부를 리턴하는 메소드
+	//회원 한명의 정보를 DB 에 저장하고 성공여부를 리턴하는 메소드
 	public boolean insert(UsersDto dto) {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		int rowCount = 0;
 		try {
 			conn = new DbcpBean().getConn();
-			String sql = "insert into users"
+			String sql = "INSERT INTO users"
 					+ " (id, pwd, email, regdate)"
-					+ " values(?, ?, ?, sysdate)";
+					+ " VALUES(?, ?, ?, SYSDATE)";
 			pstmt = conn.prepareStatement(sql);
 			//실행할 sql 문이 미완성이라면 여기서 완성
 			pstmt.setString(1, dto.getId());
 			pstmt.setString(2, dto.getPwd());
-			pstmt.setString(3, dto.getEmail());			
+			pstmt.setString(3, dto.getEmail());
 			//sql 문을 수행하고 변화된(추가, 수정, 삭제된) row 의 갯수 리턴 받기
 			rowCount = pstmt.executeUpdate();
 		} catch (SQLException se) {
--- Step05Final/src/main/webapp/users/private/pwd_updateform.jsp
+++ Step05Final/src/main/webapp/users/private/pwd_updateform.jsp
@@ -4,37 +4,36 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
 <title>/users/private/pwd_updateform.jsp</title>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
 </head>
 <body>
 	<div class="container">
-		<h1>비밀번호 변경 페이지</h1>
+		<h1>비밀 번호 수정 페이지</h1>
 		<form action="update.jsp" method="post" id="myForm">
-			<div class="mb-2">
-				<label class="form-label" for="pwd">현재 비밀번호 입력</label>
-				<input class="form-control" type="password" name="pwd" id="pwd" />
+			<div>
+				<label class="form-label" for="pwd">기존 비밀번호</label>
+				<input class="form-control" type="password" name="pwd" id="pwd"/>
 			</div>
-			<div class="mb-2">
-				<label class="form-label" for="newPwd">새 비밀번호 입력</label>
-				<input class="form-control" type="password" name="newpwd" id="newPwd" />
+			<div>
+				<label class="form-label" for="newPwd">새 비밀번호</label>
+				<input class="form-control" type="password" name="newPwd" id="newPwd"/>
 			</div>
-			<div class="mb-2">
+			<div>
 				<label class="form-label" for="newPwd2">새 비밀번호 확인</label>
-				<input class="form-control" type="password" id="newPwd2" />
+				<input class="form-control" type="password" id="newPwd2"/>
 			</div>
 			<button type="submit">확인</button>
-			<button type="reset">리셋</button>
+			<button type="reset">리셋</button>		
 		</form>
 	</div>
 	<script>
-		// 폼에 submit 이벤트가 일어났을 때 실행할 함수를 등록하고
+		//폼에 submit 이벤트가 일어났을때 실행할 함수를 등록하고 
 		document.querySelector("#myForm").addEventListener("submit", (e)=>{
-			// 입력한 새 비밀번호 2개를 읽어와서
+			//입력한 새 비밀번호 2개를 읽어와서
 			let pwd1=document.querySelector("#newPwd").value;
 			let pwd2=document.querySelector("#newPwd2").value;
-			// 만일 새 비밀번호와 비밀번호 확인이 일치하지 않으면 폼 전송을 막는다.
+			//만일 새 비밀번호와 비밀번호 확인이 일치하지 않으면 폼 전송을 막는다.
 			if(pwd1 != pwd2){
 				alert("비밀번호를 확인 하세요!");
 				e.preventDefault();
@@ -43,4 +42,3 @@
 	</script>
 </body>
 </html>
-
